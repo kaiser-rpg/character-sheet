1603b344cee08e4fb06c189c7bc2fc865893c23b
Alex Berns <eberron94@gmail.com>
2019-08-29 13:57:08 -0500

    added custom theme

--- src/renderer.js
+++ src/renderer.js
@@ -3,7 +3,7 @@ import * as ReactDOM from "react-dom";
 import {Provider} from "react-redux";
 import App from "./components/App";
 import {store} from "./reducers";
-
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
 
 const fs = window.require('fs');
 // fs.readFile('data/kara.json', loadCharacterData);
@@ -11,15 +11,27 @@ const fs = window.require('fs');
 // store.subscribe(() =>
 // );
 
+const theme = createMuiTheme({
+    breakpoints: {
+        values: {
+            xs: 200,
+            sm: 400,
+            md: 600,
+            lg: 800,
+            xl: 1000,
+        }
+    }
+});
+
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>
+    <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </MuiThemeProvider>
     , document.getElementById('app')
-)
-
-// store.dispatch(setCharacterName("Luara"));
+);
 
 
 
